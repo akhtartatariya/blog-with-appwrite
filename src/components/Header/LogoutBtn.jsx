69920fba06f1135f49fa83a6af4e51644437e2c8
Akhtar <137376031+akhtartatariya@users.noreply.github.com>
2024-10-02 11:38:15 +0000

    navigation add

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -3,10 +3,13 @@ import Button from '../Button'
 import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
 import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 const LogoutBtn = () => {
   const dispatch = useDispatch()
+  const navigate=useNavigate()
   const logoutHander = () => {
     authService.logout().then(() => {
+      navigate('/login')
       dispatch(logout())
     })
   }
